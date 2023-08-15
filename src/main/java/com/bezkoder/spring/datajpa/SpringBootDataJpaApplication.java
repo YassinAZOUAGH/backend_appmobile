e6e361c07d8db40470b2bb411d25f956c36957b1
YassinAZOUAGH <54190@etu.he2b.be>
2023-08-15 12:18:11 +0200

    Update SpringBootDataJpaApplication.java

--- src/main/java/com/bezkoder/spring/datajpa/SpringBootDataJpaApplication.java
+++ src/main/java/com/bezkoder/spring/datajpa/SpringBootDataJpaApplication.java
@@ -2,7 +2,9 @@ package com.bezkoder.spring.datajpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(origins = "http://smarteasyorders.com")
 @SpringBootApplication
 public class SpringBootDataJpaApplication {
 
