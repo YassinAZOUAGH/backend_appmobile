4fa613a7fd4c8c107eab357ae9ca366b6f5d023d
YassinAZOUAGH <54190@etu.he2b.be>
2023-08-27 17:02:52 +0200

    Update AuthController.java

--- src/main/java/com/bezkoder/spring/datajpa/controller/AuthController.java
+++ src/main/java/com/bezkoder/spring/datajpa/controller/AuthController.java
@@ -19,7 +19,9 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
-@CrossOrigin(origins = "*", maxAge = 3600)
+//@CrossOrigin(origins = "*", maxAge = 3600)
+@CrossOrigin(origins = "https://smarteasyorders.com", maxAge = 3600)
+
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
